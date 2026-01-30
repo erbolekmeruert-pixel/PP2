b6fc074bf2b358527b4cb772b7d90d7cf23b17f5
erbolekmeruert-pixel <erbolekmeruert@gmail.com>
2026-01-30 22:13:49 +0500

    Variables

---
+++ 7.py
@@ -0,0 +1,21 @@
+x = 7
+y = "Meru"
+print(x)
+print(y)
+
+x = "Yerb"
+print(x)
+
+x = str(8)
+y = int(8)
+z = float(8)
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
