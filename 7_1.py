5ac58b0599a7322e399a86de86e5231b1c3feec2
erbolekmeruert-pixel <erbolekmeruert@gmail.com>
2026-01-31 09:00:27 +0500

    Names values vari

---
+++ 7_1.py
@@ -0,0 +1,36 @@
+myvar = "Noah"
+my_var = "Will"
+_my_var = "Finn"
+myVar = "Lucas"
+MYVAR = "Dastin"
+myvar2 = "Saddie"
+
+x, y, z = "aka", "kiro", "orange"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Mikasa"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Levi"
+y = "is"
+z = "awesome"
+print(x + y + z)
+
+x = 8
+y = "Armin"
+print(x, y)
